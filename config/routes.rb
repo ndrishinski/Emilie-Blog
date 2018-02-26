1d3e93dbb97251a9e12040bcf42faa8e558b1913
Nick Drishinski <ndrishinski@yahoo.com>
2018-02-25 17:57:50 -0700

    Implemented bootstrap styles into nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
 
+  devise_for :models
   get "about", to:'pages#about'
 
   get "contact", to:"pages#contact"
 
   get "portfolio", to:'pages#portfolio'
 
+  resources :blogs
+
   root "pages#home"
+
+  root to: "home#index"
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 end
